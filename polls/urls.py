5a3f18e3b3c155eb928b9a03600c6ce839931d86
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-26 18:15:40 +0300

    part 3 done, url, template, 404, views

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
+from .views import detail, results, vote
 from django.urls import  path
 
 from polls.views import index
 
+app_name= "polls" # hreflerde verilen "url" nin hangi app i çağıracağını belirtir
 urlpatterns = [
-    path("", index)
+    path("", index),
+    path("<int:question_id>/", detail, name="detail"),
+    path("<int:question_id>/results", results, name="results"),
+    path("<int:question_id>/vote", vote, name="vote"),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,32 @@
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from polls.models import Question
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hello")
+""" def index(request): #long method
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # output= ", ".join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    context={
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request)) """
+def index(request): # short method
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context={
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
