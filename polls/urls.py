7ff10a82c64271e766b7c3b704702e79fb1db9dd
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-27 14:58:37 +0300

    tutorial 4 completed, generic views (class views)

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,16 @@
-from .views import detail, results, vote
+from .views import DetailView, IndexView, ResultsView, detail, results, vote
 from django.urls import  path
 
 from polls.views import index
 
 app_name= "polls" # hreflerde verilen "url" nin hangi app i çağıracağını belirtir
 urlpatterns = [
-    path("", index),
-    path("<int:question_id>/", detail, name="detail"),
-    path("<int:question_id>/results", results, name="results"),
+    # path("", index),
+    # path("<int:question_id>/", detail, name="detail"),
+    # path("<int:question_id>/results", results, name="results"),
+    path('', IndexView.as_view(), name='index'),
+    path('<int:pk>/', DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', ResultsView.as_view(), name='results'),
     path("<int:question_id>/vote", vote, name="vote"),
 
 ]
